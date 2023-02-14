34717d236726e6afae65461179da28a0bc30c948
dmavin <davin0130@naver.com>
2023-02-14 17:45:27 +0900

    Modify:main, DAO:record voice log

--- DAO.py
+++ DAO.py
@@ -6,6 +6,7 @@ conn, cursor, result = TJ_DBConnection.dbconn()
 select_all_user_sql  = "SELECT * FROM user_info LIMIT 0, 20"
 select_id_user_sql   = "SELECT * FROM user_info WHERE user_id=%s"
 insert_user_sql      = "INSERT INTO user_info(user_id, tag, join_date) VALUES(%s, %s, %s)"
+select_admin_sql     = "SELECT * FROM user_info WHERE admin=1"
 
 # chat_sql
 select_all_chat_sql  = "SELECT * FROM chat_info LIMIT 0, 20"
@@ -14,10 +15,12 @@ insert_chat_sql      = "INSERT INTO chat_info(user_id, channel, in_time) VALUES(
 update_chat_sql      = "UPDATE chat_info SET out_time=%s where user_id=%s and channel=%s"
 
 # voice_sql
-select_all_voice_sql = "SELECT * FROM voice_info LIMIT 0, 20"
+select_all_voice_sql  = "SELECT * FROM voice_info LIMIT 0, 20"
 select_id_voice_sql   = "SELECT * FROM voice_info WHERE user_id=%s and channel=%s"
 insert_voice_sql      = "INSERT INTO voice_info(user_id, channel, in_time) VALUES(%s, %s, %s)"
 update_voice_sql      = "UPDATE voice_info SET out_time=%s where user_id=%s and channel=%s"
+reset_voice_sql       = "UPDATE voice_info SET in_time=%s, out_time=null where user_id=%s and channel=%s"
+
 
 # =================================================================
 
@@ -41,6 +44,14 @@ class User:
         cursor.execute(insert_user_sql, value)
         cursor.fetchall()
         conn.commit()
+    
+    # select_admin
+    def searchAdmin():
+        cursor.execute(select_admin_sql)
+        admin_list = cursor.fetchall()
+        conn.commit()
+        return admin_list
+
 
 # =================================================================
 
@@ -101,5 +112,12 @@ class Voice:
     def updateVoice(current_time, user_id, channel):
         value = (current_time, user_id, channel)
         cursor.execute(update_voice_sql, value)
+        cursor.fetchall()
+        conn.commit()
+    
+    # reset_voice_sql
+    def resetVoice(current_time, user_id, channel):
+        value = (current_time, user_id, channel)
+        cursor.execute(reset_voice_sql, value)
         cursor.fetchall()
         conn.commit()
---
+++ mainBot.py
@@ -0,0 +1,111 @@
+import discord
+import TJ_DBConnection, DAO, Admin
+from datetime import datetime
+from discord.ext import commands
+
+
+# 사용자별 채팅, 음성을 모조리 기록해서 
+# DB에 저장하고 
+# 한 달에 한 번 인원정리를 하는데,
+# 특정 음성채널 입장 누적 시간이 1시간 미만인 
+# 사람들을 걸러내는 봇 쓰고있어
+# 내가 지금 만들어놓은건 버그도 많은 것 같고 
+# 코드 정리1도 안 되어 있어서 
+
+discord_token = "MTA2OTg1MDcxMDgwMTM5OTgyOA.GZzP1v.yNz4JUhZtemCbAPagldRx4ZDibSLPZjeBJ6KgU"
+
+intents = discord.Intents.all()
+intents.members = True
+intents.guilds = True
+
+client = discord.Client(intents=intents)
+
+bot = commands.Bot(command_prefix = '$', intents=intents)
+
+db = TJ_DBConnection.dbconn()
+db
+
+@client.event
+async def on_ready():
+    print('Bot name: {}'.format(client.user.name))  
+    print('Bot ID: {}'.format(client.user.id)) 
+
+
+
+# 사용자 채팅 기록
+@client.event
+async def on_message(message):
+    
+    content = message.content
+    user = message.author
+    channel = message.channel
+    guild = channel.guild
+
+    current_time = "%s-%s-%s %s:%s:%s" % (
+                    datetime.now().year, 
+                    datetime.now().month, 
+                    datetime.now().day, 
+                    datetime.now().hour, 
+                    datetime.now().minute, 
+                    datetime.now().second
+                )
+
+
+    # if content.startswith('hello'):
+    #     await channel.send("Hello! "+str(user))
+
+    # elif content == "$user":
+    #     member_num = 0
+    #     to_send_users = f'서버명 [{guild.name}]: 멤버 [{guild.member_count}명]\n\n'
+    #     for member in guild.members:
+    #         member_num += 1
+    #         to_send_users += f'{member_num}. {member}\n'
+    #     await channel.send(to_send_users)
+    
+    # elif content == "$채팅기록":
+    #     chat_log = f'{user} | {content}'
+    #     await channel.send(chat_log)
+
+    if user.id != client.user.id:
+        if len(DAO.Chat.selectIDChat(user.id, channel.id)) == 0:
+                DAO.Chat.insertChat(user.id, channel.id, current_time)
+        else:
+            DAO.Chat.updateChat(current_time, user.id, channel.id)
+        
+        # 관리 기능
+        if Admin.adminList.adminList(user) == True:
+            Admin.adminList.searchComment(content)
+        
+            
+
+    else:
+        print("this is bot")
+        return
+
+# 사용자 voice 기록
+@client.event
+async def on_voice_state_update(member, before, after):
+
+    current_time = "%s-%s-%s %s:%s:%s" % (
+                    datetime.now().year, 
+                    datetime.now().month, 
+                    datetime.now().day, 
+                    datetime.now().hour, 
+                    datetime.now().minute, 
+                    datetime.now().second
+                )
+
+    if member.id != client.user.id:
+        if after.channel == None:
+            DAO.Voice.updateVoice(current_time, member.id, before.channel.id)
+        else:
+            if len(DAO.Voice.selectIDVoice(member.id, after.channel.id)) == 0:
+                DAO.Voice.insertVoice(member.id, after.channel.id, current_time)
+            else:
+                DAO.Voice.resetVoice(current_time, member.id, after.channel.id)
+
+
+
+
+
+client.run(discord_token)
